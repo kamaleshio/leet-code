051930e2199f8741c34d9816e9205cb986bc3086
Kamalesh T <kamaleshthirunavukkarasu57@gmail.com>
2026-09-25 11:03:04 +0530

    Time: 12 ms (15.70%) | Memory: 19.1 MB (94.96%) - LeetSync

---
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestSumAfterKNegations(self, nums: list[int], k: int) -> int:
+        for i in range(k):
+            nums.sort()
+            nums[0]=-nums[0]
+        return sum(nums)
+        
